e3fd00adb2cb1a5e00a378c463ed75b8502ac0bd
tk51486 <tk51486@uga.edu>
2024-09-18 14:51:54 -0400

    trying to deblock

--- Assembly/Stopwatch/scantest.s
+++ Assembly/Stopwatch/scantest.s
@@ -1,22 +1,42 @@
 
-
-
 		.global main
 		.func main
 main:
 		PUSH {LR}
-		LDR R0, =format
+		LDR R0, =0
+		BL E4235_KYBdeblock
+		
+		BL read
+		
+read:
+		LDR R0, =input
+		LDRB R4, [R0]
+		LDRB R5, [R0]
+		
+loop:	LDR R0, =format
 		LDR R1, =input
 		BL scanf
 		
-		LDRB R2, =compare
-		@LDRB R3, =compare
-		@LDRB R3, [R1]
+		LDR R0, =input
+		LDRB R5, [R0]
+		CMP R4, R5
+		
+		LDR R0, =test
+		BL printf
+		BEQ loop
+		BNE check
+		
+check:	
+		LDR R0, =input
+		LDRB R3, [R0]
+		LDR R0, =compare
+		LDRB R2, [R0]
+		
 		CMP R2, R3
 		BNE _exit
 		LDR R0, =equal
 		BL printf
-
+		
 _exit:
         POP {PC}
         MOV PC, LR
@@ -25,11 +45,13 @@ _exit:
 format:
 		.asciz "%c"
 input:
-		.byte 0
+		.byte 'c'
 compare:
 		.byte 'c'
 equal:
-		.asciz "c was pressed"
-compare2:
-		.byte 'c'
+		.asciz "c was pressed\n"
+test:
+		.asciz "looping\n"
+null:
+		.byte '\0'
 	
