2360c5178b97821629f8b352f37a8afb50a03664
aam08331 <112673088+aam08331@users.noreply.github.com>
2024-11-06 12:00:50 -0500

    Update etothex.c
    comments added

--- Assembly/Floating Point/etothex.c
+++ Assembly/Floating Point/etothex.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-extern float e_factorial(float);
-extern float e_expo(float, int);
+extern float e_factorial(float); //setting up external factorial assembly function
+extern float e_expo(float, int); //setting up external exponent assembly function
 
 int main(void)
 {
-	printf("Calculate e^x. Enter x: ");	
-	float base;
-	scanf("%f", &base);
-	float result = 1 + base + (e_expo(base, 2)/e_factorial(2)) + (e_expo(base, 3)/e_factorial(3)) + (e_expo(base, 4)/e_factorial(4)) + (e_expo(base, 5)/e_factorial(5));
+	printf("Calculate e^x. Enter x: ");	//printing welcome message
+	float base; //initializing base for storage after input
+	scanf("%f", &base); //reading base input
+	float result = 1 + base + (e_expo(base, 2)/e_factorial(2)) + (e_expo(base, 3)/e_factorial(3)) + (e_expo(base, 4)/e_factorial(4)) + (e_expo(base, 5)/e_factorial(5)); //performing approximation based on the first five terms of the series
 	
-	printf("e^%f = %f", base, result);
+	printf("e^%f = %f", base, result); //printing base along with calculated result
 	
 	return 0;
 }
