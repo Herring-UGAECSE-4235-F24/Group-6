cdc86b538985fc4a0b2a1eaf044ae6f19d4dcb9d
tk51486 <tk51486@uga.edu>
2024-11-19 15:29:44 -0500

    i2cdriver done

--- C/I2CDriver/Driver.h
+++ C/I2CDriver/Driver.h
@@ -2,7 +2,7 @@
 #define driver_H
 
 int i2c_setup(int sda, int scl);
-int i2c_write(int bytes[8]);
+int i2c_write(char address[8], int bytes[8]);
 int i2c_send(char byte[]);
-void i2c_read(char thing[][8]);
+void i2c_read(char address[8], char thing[][8]);
 #endif
